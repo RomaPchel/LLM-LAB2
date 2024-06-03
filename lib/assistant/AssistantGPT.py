fa50b1dfad026ecf53475b916751c922454c2fbc
romapchel <notpchelintsevroma@gmail.com>
2024-06-03 15:13:29 +0300

    1

--- lib/assistant/AssistantGPT.py
+++ lib/assistant/AssistantGPT.py
@@ -1,14 +1,41 @@
 import spacy
 from spacy.language import Language
+from spacy.tokens import Doc
 from lib.db.config import conn
 from lib.lemmas.lemmas import medication_lemmas
 from lib.models.Medication import Medication
 
+from openai import OpenAI
 
-class Assistant:
+client = OpenAI(api_key='')
+
+def detect_intent_with_gpt(query):
+    prompt = (f"Please classify the following query it is going to be a question from a customer."
+              f"You should match query to one of the categories:"
+              f"add_query (if the user wants to add a medication),"
+              f"recommendation_query (if the client states a symptom or asks how to treat some illness),"
+              f"price_query (if the client wants to get information about price),"
+              f"instruction_query (if the client wants to get instructions on how to use a medication),"
+              f"Provide ONLY a direct answer with the format: "
+              f"'category' (without quotes) - where category is your match."
+              f"Query: {str(query)}")
+
+    response = client.chat.completions.create(
+        messages=[
+            {"role": "system", "content": prompt},
+            {"role": "user", "content": str(query)}
+        ],
+        model="gpt-3.5-turbo"
+    )
+
+    output = response.choices[0].message.content.strip()
+    return output
+
+class AssistantGPT:
     def __init__(self):
         try:
             self.__nlp = spacy.load("uk_core_news_sm")
+            Doc.set_extension("user_intent", default="", force=True)
             self.__nlp.add_pipe("custom_pipe", last=True)
             cursor = conn.cursor()
             cursor.execute("SELECT * FROM medication")
@@ -18,22 +45,9 @@ class Assistant:
         except Exception as e:
             print(e)
 
-    def __detect_intent(self, doc):
-        for token in doc:
-            if token.text.lower() in ["ціна", "ціни", "ціною", "вартість", "вартіст"]:
-                return "price_query"
-            elif token.text.lower() in ["застосовувати", "приймати", "застосування", "приймання"]:
-                return "instruction_query"
-            elif token.text.lower() in ["додати", "додай", "запиши", "записати"]:
-                return "add_query"
-            elif token.text.lower() in ["рекомендація", "рекомендації", "порада", "поради"]:
-                return "recommendation_query"
-        return "general_query"
-
     def process_query(self, query):
         doc = self.__nlp(query)
-        intent = self.__detect_intent(doc)
-        print(intent)
+        intent = self.use_gpt(query)
         if intent == "price_query":
             return self.__process_price_query(doc)
         elif intent == "instruction_query":
@@ -73,12 +87,10 @@ class Assistant:
 
     def __process_instruction_query(self, doc):
         self.last_intent = "instruction"
-        medication = self.__get_specific_medication_if_mentioned(doc)
+        medication = self.last_medication
 
         if not medication:
-            medication = self.last_medication
             print("Інформація не знайдена")
-
         print(f"Інструкція: {medication.additionalInfo}")
 
     def __process_recommendation_query(self, doc):
@@ -87,15 +99,12 @@ class Assistant:
 
         if not symptoms:
             print("Вибачте, я не можу надати рекомендації без вказівки на симптоми.")
-            return
 
-        print(symptoms)
         recommended_medications = self.__find_medications_by_symptoms(symptoms)
 
         if not recommended_medications:
             print("На жаль, за вашими симптомами не знайдено рекомендованих препаратів.")
-            return
-        print(recommended_medications)
+
         print(f"Рекомендовані препарати для ваших симптомів: {', '.join(recommended_medications)}")
 
 
@@ -146,6 +155,28 @@ class Assistant:
         self.medications_list.append((name, price, usedFor, additionalInfo, symptoms))
         print("Препарат успішно додано до бази даних.")
 
+    def use_gpt(self, query):
+        prompt = (f"Please classify the following query it is going to be a question from a customer."
+                  f"You should match query to one of the categories:"
+                  f"add_query (if the user wants to add a medication),"
+                  f"recommendation_query (if the client states a symprom or asks how to treat some illness),"
+                  f"price_query (if the client wants to get information about price),"
+                  f"instruction_query (if the client wants to get instructions on how to use a medication),"
+                  f"Provide ONLY a direct answer with the format: "
+                  f"'category' (without quotes) - where category is your match."
+                  f"Query: {query}")
+
+        response = client.chat.completions.create(
+            messages=[
+                {"role": "system", "content": prompt},
+                {"role": "user", "content": query}
+            ],
+            model="gpt-3.5-turbo"
+        )
+
+        output = response.choices[0].message.content.strip()
+        print(output)
+        return output
 
 
 
@@ -155,6 +186,7 @@ def custom_lemmatizer(doc):
     for token in doc:
         if token.text.lower() in lemma_map:
             token.lemma_ = lemma_map[token.text.lower()]
-            print(token)
-
+    doc._.user_intent = detect_intent_with_gpt(doc.text)
     return doc
+
+
